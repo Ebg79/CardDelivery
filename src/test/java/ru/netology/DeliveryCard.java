762c9f61b1abca7c79148d2a388dee156df76391
Evgeniy <ebg79@yandex.ru>
2023-08-09 16:47:11 +0300

    Add Modification

--- src/test/java/ru/netology/DeliveryCard.java
+++ src/test/java/ru/netology/DeliveryCard.java
@@ -1,54 +1,72 @@
 package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryCard {
+    SelenideElement form = $("form");
+    SelenideElement city = form.$("[data-test-id=city] input");
+    SelenideElement cityClick = $(".menu");
+    SelenideElement cityChoose = $(byText("Казань"));
+    SelenideElement date = form.$("[data-test-id=date] input");
+    SelenideElement popupDate = $(".calendar");
+    SelenideElement deliveryDate = $(byText("8"));
+    SelenideElement name = form.$("[data-test-id=name] input");
+    SelenideElement phone = form.$("[data-test-id=phone] input");
+    SelenideElement agreement = form.$("[data-test-id=agreement]");
+    SelenideElement button = $$("button").find(exactText("Забронировать"));
+    SelenideElement notification = $("[data-test-id=notification]");
 
 
-class DeliveryCard {
-    String[] monthNames = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
-    String date;
-    String month;
-    String day;
+
+    private String getFutureDate (int plusDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate controlDate = currentDate.plusDays(plusDate);
+        String formattedControlDate = controlDate.format(formatter);
+        return formattedControlDate;
+    }
+
 
     @BeforeEach
-    void setUp() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.DAY_OF_MONTH, 100);
-        date =  new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime());
-        month = monthNames[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.YEAR);
-        day = Integer.toString(calendar.get(Calendar.DATE));
+    void openHost() {
         open("http://localhost:9999");
     }
 
     @Test
-    @DisplayName("Должен забронировать встречу при вводе валидных данных")
-    void shouldRegisterCardDelivery() {
-        $("span[data-test-id='city'] input").setValue("Ка");
-        $$("div.popup__content div").find(exactText("Казань")).click();
-        $("span[data-test-id='date'] button").click();
-
-        while (!$("div.calendar__name").getText().equals(month)) {
-            $$("div.calendar__arrow.calendar__arrow_direction_right").get(1).click();
-        }
-
-        $$("table.calendar__layout td").find(text(day)).click();
-        $("span[data-test-id='name'] input").setValue("Кузьмин Евгений");
-        $("span[data-test-id='phone'] input").setValue("+79150560000");
-        $("label[data-test-id='agreement']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $("div.notification__content").waitUntil(text("Встреча успешно забронирована на " + date),
-                15000);
+    void shouldTest() {
+        city.setValue("Новосибирск");
+        cityClick.waitUntil(exist, 5000).click();
+        date.doubleClick().sendKeys(Keys.BACK_SPACE);
+        String futureDay = getFutureDate(3);
+        date.setValue(futureDay);
+        name.setValue("Кузьмин Евгений");
+        phone.setValue("+79150560000");
+        agreement.click();
+        button.click();
+        notification.waitUntil(visible, 15000);
     }
-
+    @Test
+    void autoPutTest() {
+        city.setValue("Ка");
+        cityClick.waitUntil(exist, 5000);
+        cityChoose.click();
+        date.click();
+        popupDate.waitUntil(exist, 5000);
+        deliveryDate.click();
+        name.setValue("Кузьмин Евгений");
+        phone.setValue("+79150560000");
+        agreement.click();
+        button.click();
+        notification.waitUntil(visible, 15000);
+    }
 }
